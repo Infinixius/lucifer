2e643721ae5dc78fc944ccc3508f0718e48010b6
Reagan <reaganmericle@outlook.com>
2021-11-13 17:47:19 -0500

    nards

--- client/scripts/KinematicBody2D.gd
+++ client/scripts/KinematicBody2D.gd
@@ -2,6 +2,10 @@ extends KinematicBody2D
 
 export (int) var speed = 200
 
+onready var _animated_sprite = $AnimatedSprite
+
+
+
 onready var tile_map = $"../TileMap"
 onready var player = self
 onready var Multiplayer = $"../Players"
@@ -14,24 +18,28 @@ var speedvel = 1000
 func get_input():
 	velocity = Vector2()
 	if Input.is_action_pressed("right"):
+		_animated_sprite.play("walk_right")
 		velocity.x += speedvel
 		bulletspawn.position = bulletspawn.get_global_position()
 		bulletspawn.position.x = 32
 		bulletspawn.position.y = 16
 		bulletspawn.rotation_degrees = 0
 	if Input.is_action_pressed("left"):
+		_animated_sprite.play("walk_left")
 		velocity.x -= speedvel
 		bulletspawn.position = bulletspawn.get_global_position()
 		bulletspawn.position.x = 0
 		bulletspawn.position.y = 16
 		bulletspawn.rotation_degrees = 180
 	if Input.is_action_pressed("down"):
+		_animated_sprite.play("walk_down")
 		velocity.y += speedvel
 		bulletspawn.position = bulletspawn.get_global_position()
 		bulletspawn.position.x = 16
 		bulletspawn.position.y = 32
 		bulletspawn.rotation_degrees = 90
 	if Input.is_action_pressed("up"):
+		_animated_sprite.play("walk_up")
 		velocity.y -= speedvel
 		bulletspawn.position = bulletspawn.get_global_position()
 		bulletspawn.position.x = 16
