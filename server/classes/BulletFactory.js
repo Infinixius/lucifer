82fd5bf7b9a015f640b480d81a6dcdd493a59f14
Infinixius <68125679+Infinixius@users.noreply.github.com>
2022-02-04 09:10:58 -0500

    v0.3 - Add BulletFactory

---
+++ server/classes/BulletFactory.js
@@ -0,0 +1,27 @@
+import Bullet from "./Bullet.js"
+
+export default class BulletFactory {
+	constructor(player) {
+		this.player = player
+		this.bullets = new Map()
+
+		this.lastShot = 0
+	}
+	createBullet(pos, direction) {
+		if (Date.now() - this.lastShot < config.shootCooldown) return
+
+		this.lastShot = Date.now()
+
+		var bullet = new Bullet(pos, direction)
+		this.bullets.set(bullet.id, bullet)
+	}
+	networkUpdate() {
+		this.bullets.forEach((bullet, id) => {
+			if (Date.now() - bullet.createdAt >= config.bulletLifespan) {
+				this.bullets.delete(id)
+				return
+			}
+			broadcast("entity_update", bullet)
+		})
+	}
+}
