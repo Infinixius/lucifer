58d8f228a777983cdd9a9d7f99c79629b146f4e6
Infinixius <68125679+Infinixius@users.noreply.github.com>
2022-02-04 09:10:49 -0500

    v0.3 - typo

--- server/classes/Client.js
+++ server/classes/Client.js
@@ -3,7 +3,7 @@ export default class Client {
 		this.ws = ws
         this.req = req
         this.id = id
-        this.ip = req.socket.remoteAddress // x-forwarded-for must be used if the server is running behind a reverse proxy
+        this.ip = req.socket.remoteAddress
 
         if (req.headers["x-forwarded-for"]) {
             this.ip = req.headers["x-forwarded-for"].split(",")[0].trim() // x-forwarded-for must be used if the server is running behind a reverse proxy
