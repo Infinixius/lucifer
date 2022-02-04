de030a379f43b2ca72bd55f8240a510726baddd1
Infinixius <68125679+Infinixius@users.noreply.github.com>
2022-02-04 09:10:43 -0500

    v0.3 - Update entity code

--- server/classes/entity.js
+++ server/classes/entity.js
@@ -1,8 +1,17 @@
-export default class Entity {
-	constructor(pos, size, rotation) {
+export class Entity {
+	constructor(type, pos, size, rotation) {
+		this.type = type
 		this.position = pos || [0, 0]
-		this.size = size || [0, 0]
+		this.size = size || [1, 1]
 		this.rotation = rotation || 0
 		this.velocity = 0
+		
+		global.uid++
+		this.id = global.uid
+		this.createdAt = Date.now()
 	}
+}
+
+export const EntityTypes = {
+	Bullet: 2
 }
