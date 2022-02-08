82afeb3d3f1ad6f251adab8a4e281191159ab29b
Infinixius <68125679+Infinixius@users.noreply.github.com>
2022-02-08 10:07:45 -0500

    v0.4 - Remove useless scripts

--- client/scripts/AudioStreamPlayer.gd
+++
@@ -1,9 +0,0 @@
-extends AudioStreamPlayer
-
-var stone_sound = preload("res://assets/sfx/step_stone.mp3")
-
-func playFootstep(type):
-	if !self.is_playing():
-		if type == "stone":
-			self.stream = stone_sound
-			self.play()
